0b38ff958c0a26778abe9fdd7808561a619f290c
Lukasz Czerwinski <wo3kie@gmail.com>
2012-03-30 21:16:25 +0200

    Unit test separate file

---
+++ pch_test.py
@@ -0,0 +1,24 @@
+from pch import\
+    parseLine
+
+# pchGenerator
+
+import unittest
+
+class TestParseLine(unittest.TestCase):
+    def test_negative( self ):
+        self.assertRaises( Exception, parseLine, "line" )
+        self.assertRaises( Exception, parseLine, ". " )
+        self.assertRaises( Exception, parseLine, "...line" )
+        self.assertRaises( Exception, parseLine, "......." )
+
+    def test_positive( self ):
+        self.assertEqual( parseLine( ". f" ), ( 1, "f" ) )
+        self.assertEqual( parseLine( ".. directory" ), ( 2, "directory" ) )
+        self.assertEqual( parseLine( "... path" ), ( 3, "path" ) )
+
+#
+# main
+#
+if __name__ == "__main__":
+    unittest.main()
