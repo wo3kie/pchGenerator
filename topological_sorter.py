0dd0e590e8cafde4c3653112a6c7605ae16ee89f
Lukasz Czerwinski <wo3kie@gmail.com>
2012-04-01 22:37:10 +0200

    topological_sorter

---
+++ topological_sorter.py
@@ -0,0 +1,41 @@
+from dag import\
+    DfsNode
+
+import copy
+
+#
+# TopologicalSorter:
+#
+class TopologicalSorter:
+    def __init__( self, dag ):
+        self._counter = 0
+
+        self.__calculatePrePostTime( dag.getRoot() )
+
+        self._sortedNodes = []
+        for item in dag.getNodes():
+            self._sortedNodes.append( item )
+
+        self._sortedNodes.sort( reverse=True, key=(lambda x: x.getPostVisit() ) )
+
+    def __calculatePrePostTime( self, node ):
+        assert node.getColor() != DfsNode.Grey, "no cycles allowed"
+
+        if node.getColor() == DfsNode.Black:
+            return
+
+        node.setColor( DfsNode.Grey )
+
+        self._counter += 1
+        node.setPreVisit( self._counter )
+
+        for child in node.getChildren():
+            self.__calculatePrePostTime( child )
+
+        node.setColor( DfsNode.Black )
+
+        self._counter += 1
+        node.setPostVisit( self._counter )
+
+    def getNodes( self ):
+        return self._sortedNodes
---
+++ topological_sorter_test.py
@@ -0,0 +1,48 @@
+import unittest
+
+from topological_sorter import\
+    TopologicalSorter
+
+from dag import\
+    Dag
+
+#
+# DfsNode
+#
+class TestSopologicalSorter( unittest.TestCase ):
+    def test_1( self ):
+        dag = Dag()
+
+        # . a
+        # . b
+        # . c
+
+        dag.add( 1, "a" )
+        dag.add( 1, "b" )
+        dag.add( 1, "c" )
+
+        tSorter = TopologicalSorter( dag )
+
+        self.assertEqual( [ i.getData() for i in tSorter.getNodes() ], [ "b", "c", "a" ] )
+
+    def test_2( self ):
+        dag = Dag()
+
+        # . a
+        # .. c
+        # .. b
+        # ... c
+
+        dag.add( 1, "a" )
+        dag.add( 2, "c" )
+        dag.add( 2, "b" )
+        dag.add( 3, "c" )
+
+        tSorter = TopologicalSorter( dag )
+
+        self.assertEqual( [ i.getData() for i in tSorter.getNodes() ], [ "a", "b", "c" ] )
+#
+# main
+#
+if __name__ == "__main__":
+    unittest.main()
