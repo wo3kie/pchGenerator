43f14e0d5521b5dafe8b4cacc238866067de4ceb
Lukasz Czerwinski <wo3kie@gmail.com>
2012-04-12 23:34:28 +0200

    Unit tests fix

--- recursive_filter_test.py
+++ recursive_filter_test.py
@@ -10,6 +10,13 @@ from recursive_filter import\
     RecursiveFilter
 
 #
+# OptionsMock
+#
+class OptionsMock:
+    def __init__( self ):
+        self.watch_header = ""
+
+#
 # TestRecursiveFilter
 #
 class TestRecursiveFilter( unittest.TestCase ):
@@ -21,30 +28,32 @@ class TestRecursiveFilter( unittest.TestCase ):
         self._c = self._dag.add( 3, "c" )
         self._c = self._dag.add( 2, "c" )
 
+        self._options = OptionsMock()
+
         self._tSorter = TopologicalSorter( self._dag )
 
     def test_1( self ):
-        rFilter = RecursiveFilter( self._tSorter, (lambda x: x.getData() == "a") )
+        rFilter = RecursiveFilter( self._tSorter, (lambda x: x.getData() == "a"), self._options )
 
         self.assertEqual( rFilter.getNodes(), [ self._a ] )
 
     def test_2( self ):
-        rFilter = RecursiveFilter( self._tSorter, (lambda x: x.getData() == "b") )
+        rFilter = RecursiveFilter( self._tSorter, (lambda x: x.getData() == "b"), self._options )
 
         self.assertEqual( rFilter.getNodes(), [ self._b ] )
 
     def test_3( self ):
-        rFilter = RecursiveFilter( self._tSorter, (lambda x: x.getData() == "c") )
+        rFilter = RecursiveFilter( self._tSorter, (lambda x: x.getData() == "c"), self._options )
 
         self.assertEqual( rFilter.getNodes(), [ self._c ] )
 
     def test_4( self ):
-        rFilter = RecursiveFilter( self._tSorter, (lambda x: True) )
+        rFilter = RecursiveFilter( self._tSorter, (lambda x: True), self._options )
 
         self.assertEqual( rFilter.getNodes(), [ self._dag.getRoot() ] )
 
     def test_5( self ):
-        rFilter = RecursiveFilter( self._tSorter, (lambda x: False) )
+        rFilter = RecursiveFilter( self._tSorter, (lambda x: False), self._options )
 
         self.assertEqual( rFilter.getNodes(), [] )
 
